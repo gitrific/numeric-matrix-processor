ad14fff2390e9c183a2b46a8b50c0bf618e0ddde
gitrific <joanna.czarnecka@hotmail.com>
2020-07-19 22:18:50 +0200

    Stage 6 implementation

--- processor.py
+++ processor.py
@@ -12,11 +12,26 @@ def get_matrix(name=""):
     return A
 
 
+def pretty_print_result(result=None):
+    if result is not None and isinstance(result, list):
+        print("The result is:")
+        # print(*[' '.join(map(str, result[i])) for i in range(len(result))], sep='\n')
+        print('\n'.join([''.join(['{:12}'.format(round(item, 4)) for item in row]) for row in result]))
+        print()
+    elif result is not None and (not isinstance(result, list)):
+        print("The result is:")
+        print(result)
+        print()
+    else:
+        print("The operation cannot be performed.")
+        print()
+
+
 def add():
     A = get_matrix("first")
     B = get_matrix("second")
     if len(A) != len(B) or len(A[0]) != len(B[0]):
-        return -1
+        return None
 
     return [[a + b for a, b in zip(A_row, B_row)] for A_row, B_row in zip(A, B)]
 
@@ -25,14 +40,18 @@ def multiply_by_constant():
     A = get_matrix()
     c = float(input("Enter constant: > "))
 
+    return _multiply_by_constant(A, c)
+
+def _multiply_by_constant(A, c):
     return [[c * a for a in A_row] for A_row in A]
+
 
 
 def multiplay_matrices():
     A = get_matrix("first")
     B = get_matrix("second")
     if len(A[0]) != len(B):
-        return -1
+        return None
     return [[sum([a * b for a, b in zip(A_row, B_col)]) for B_col in zip(*B)] for A_row in A]
 
 
@@ -51,7 +70,7 @@ def transpose():
         return t_vertical(T)
     elif choice == 4:
         return t_horizontal(T)
-    return -1
+    return None
 
 
 def t_main_diag(A):
@@ -69,9 +88,11 @@ def t_horizontal(A):
 def t_vertical(A):
     return [[a for a in A_row[::-1]] for A_row in A]
 
+
 def determinant():
     A = get_matrix()
-    return _determinant(A)
+    pretty_print_result(_determinant(A))
+
 
 def _determinant(A):
     if len(A) == len(A[0]) == 1:
@@ -81,9 +102,31 @@ def _determinant(A):
     det = 0
 
     for i in range(len(A)):
-        det += (-1) ** i * A[0][i] * _determinant([[A[k][l] for l in range(len(A)) if l != i] for k in range(1, len(A))])
+        det += (-1) ** i * A[0][i] * _determinant(
+            [[A[k][l] for l in range(len(A)) if l != i] for k in range(1, len(A))])
 
     return det
+
+def cofactor_matrix(A):
+    Matrix = [[0 for x in range(len(A))] for y in range(len(A))]
+
+    for i in range(len(A)):
+        for j in range(len(A)):
+            Matrix[i][j] = (-1) ** (i+j) * _determinant([[A[k][l] for l in range(len(A)) if l != j] for k in range(len(A)) if k != i])
+    return Matrix
+
+def inverse():
+    A = get_matrix()
+    det = _determinant(A)
+    if det == 0:
+        print("This matrix doesn't have an inverse.")
+    else:
+        result = cofactor_matrix(A)
+        result = t_main_diag(result)
+        result = _multiply_by_constant(result, 1/det)
+        # result = [[round(r, 4) for r in res_row] for res_row in result]
+        pretty_print_result(result)
+
 
 def menu():
     print("1. Add matrices")
@@ -91,6 +134,7 @@ def menu():
     print("3. Multiply matrices")
     print("4. Transpose matrix")
     print("5. Calculate a determinant")
+    print("6. Inverse matrix")
     print("0. Exit")
     return int(input("Your choice: > "))
 
@@ -98,26 +142,17 @@ def menu():
 answer = menu()
 
 while answer != 0:
-    result = -1
     if answer == 1:
-        result = add()
+        pretty_print_result(add())
     elif answer == 2:
-        result = multiply_by_constant()
+        pretty_print_result(multiply_by_constant())
     elif answer == 3:
-        result = multiplay_matrices()
+        pretty_print_result(multiplay_matrices())
     elif answer == 4:
-        result = transpose()
+        pretty_print_result(transpose())
     elif answer == 5:
-        result = determinant()
-    if result != -1 and isinstance(result, list):
-        print("The result is:")
-        print(*[' '.join(map(str, result[i])) for i in range(len(result))], sep='\n')
-        print()
-    elif result != -1 and (not isinstance(result, list)):
-        print("The result is:")
-        print(result)
-        print()
-    else:
-        print("The operation cannot be performed.")
-        print()
+        determinant()
+    elif answer == 6:
+        inverse()
+
     answer = menu()
